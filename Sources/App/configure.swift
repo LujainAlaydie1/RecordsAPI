2d65eabe2f2dcbe3337b29a83e25293496a012e0
Lujain Alaydie <96392905+LujainAlaydie1@users.noreply.github.com>
2024-02-20 15:43:04 +0300

    idk

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,28 +7,11 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-<<<<<<< Updated upstream
-
-=======
     app.databases.use(.postgres (configuration: SQLPostgresConfiguration(hostname: "localhost",
                                                                username: "postgres", password: "",
                                                                database: "my_records",
                                                                tls: .prefer(try .init(configuration:.clientDefault)))), as: .psql)
                       
->>>>>>> Stashed changes
-//    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-//        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-//        tls: .prefer(try .init(configuration: .clientDefault)))
-//    ), as: .psql)
-<<<<<<< Updated upstream
-    app.databases.use(.postgres (configuration: SQLPostgresConfiguration(hostname: "localhost",
-                                                               username: "postgres", password: "",
-                                                               database: "my_records",
-                                                               tls: .prefer(try .init(configuration:.clientDefault)))), as: .psql)
 
     //CREATE ALL TABLES
     app.migrations.add(CreateCustomer())
@@ -38,10 +21,6 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(CreateSalesItems())
     app.migrations.add(CreateCustomersGenres())
     try await app.autoMigrate()
-    
-=======
->>>>>>> Stashed changes
-
 
     // register routes
     try routes(app)
